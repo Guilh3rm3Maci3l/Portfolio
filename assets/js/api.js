e728dbf9e7bfc6f334f7816ed12548f8e090173c
Guilh3rm3Maci3l <116690354+Guilh3rm3Maci3l@users.noreply.github.com>
2025-01-28 15:07:28 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://Guilh3rm3Maci3l.io/Portfolio/data/profile.json';
+    const url = 'https://Guilh3rm3Maci3l.io/Portfolio/assets/js/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
